e52f143194c65292b72565e4c00807ec26b5d465
Ahmed Hashim <ahmedha4im7@gmail.com>
2025-10-28 00:18:37 +0300

    Add Celery beat schedule for notify_sending tasks

--- project/celery.py
+++ project/celery.py
@@ -20,3 +20,23 @@ app.autodiscover_tasks()
 @app.task(bind=True, ignore_result=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
+
+
+
+from celery.schedules import crontab  
+app.conf.beat_schedule = {
+    'run-notify-every-30-seconds': {
+        'task': 'app.tasks.notify_sending',
+        'schedule': 30.0,  # كل 30 ثانية
+        'args': (5, 7),    # باراميترات للمهمة
+    },
+    'run-every-midnight': {
+        'task': 'app.tasks.notify_sending',
+        'schedule': crontab(hour=0, minute=0),  # كل يوم الساعة 00:00
+        'args': (10, 20),
+    },
+}
+
+# celery -A project beat -l info
+# if u change the schedule and want to apply it without stopping the beat service, run:
+# rm celerybeat-schedule
